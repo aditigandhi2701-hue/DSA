c4bbd9d63bb66935f3c8ecada075c7b8fa5470a4
Aditi Anand Gandhi <aditigandhi2701@gmail.com>
2026-08-31 14:51:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.7%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double power(double x, long long n) {
+        // Base case
+        if (n == 0)
+            return 1;
+
+        // Recursive call
+        double half = power(x, n / 2);
+
+        // If n is even
+        if (n % 2 == 0)
+            return half * half;
+
+        // If n is odd
+        return half * half * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        // Negative power
+        if (N < 0) {
+            return 1.0 / power(x, -N);
+        }
+
+        return power(x, N);
+    }
+};
