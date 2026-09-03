08dfca74558578a40e05d72b917b9384ba54c9af
Aditi Anand Gandhi <aditigandhi2701@gmail.com>
2026-09-03 22:28:04 +0530

    Time: 2 ms (80.09%), Space: 15.6 MB (50.31%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    void generate(int n, int open, int close,
+                  string s, vector<string>& ans) {
+
+        // Base case
+        if (s.length() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            generate(n, open + 1, close, s + "(", ans);
+        }
+
+        // Add ')'
+        if (close < open) {
+            generate(n, open, close + 1, s + ")", ans);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+
+        vector<string> ans;
+
+        generate(n, 0, 0, "", ans);
+
+        return ans;
+    }
+};
