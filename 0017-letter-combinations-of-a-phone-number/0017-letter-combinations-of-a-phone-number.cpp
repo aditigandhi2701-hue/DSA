06e139b0b219ecc8eecf0a6589b3013c8635f16d
Aditi Anand Gandhi <aditigandhi2701@gmail.com>
2026-09-07 14:27:36 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18.78%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    vector<string> letters = {
+        "", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    void solve(string digits, int index, string current) {
+
+        // Base case
+        if (index == digits.length()) {
+            ans.push_back(current);
+            return;
+        }
+
+        // Get letters for current digit
+        string s = letters[digits[index] - '0'];
+
+        // Try every letter
+        for (char ch : s) {
+
+            current.push_back(ch);
+
+            solve(digits, index + 1, current);
+
+            // Backtrack
+            current.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+
+        if (digits.empty())
+            return {};
+
+        solve(digits, 0, "");
+
+        return ans;
+    }
+};
